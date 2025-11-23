77152396c6767433bc338664c7c829b30b8f5ae1
albertying <albertlying@gmail.com>
2025-11-23 12:03:52 -0500

    setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import Form from "./routes/Form.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Form />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/network" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
